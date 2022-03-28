35fa9c5c3c398ca1de4887c472e16b89c42eb7ba
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-28 06:39:32 +0530

    Slight modification

--- Question 3/deque.java
+++ Question 3/deque.java
@@ -5,11 +5,6 @@ public class Main
 		String a = "12345*+-+";
 		Deque<Integer> q = new LinkedList<>();
 		for(int i=0;i<a.length();i++){
-		  //  System.out.println(q);
-		    if(a.charAt(i)>=48 && a.charAt(i)<57){
-		        q.addLast(a.charAt(i)-48);
-		    }
-		    else{
 		        switch(a.charAt(i)){
 		            case '+':
 		                q.addFirst(q.remove()+q.remove());
@@ -23,8 +18,11 @@ public class Main
 		            case '/':
 		                q.addFirst(q.remove()/q.remove());
 		                break;
+		            default:
+		                if(a.charAt(i)>=48 && a.charAt(i)<57){
+            		        q.addLast(a.charAt(i)-48);
+            		    }
 		        }
-		    }
 		}
 		System.out.println(q.peek());
 	}
